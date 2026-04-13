e214d664055bf682b1e7f4b2b8477c139aedf6bc
tingyang sun <tfs5679@psu.edu>
2026-04-13 09:50:42 -0400

    tingyang: update test code

--- run_parameter_c_optimization.m
+++
@@ -1,9 +0,0 @@
-% Wrapper script to run parameter c optimization test
-% This script adds necessary paths and calls the test
-
-% Add paths
-addpath(genpath('src'));
-addpath('config');
-
-% Run the test
-test_parameter_c_optimization();
